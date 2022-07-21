7755e94b5d9bef222e473a85719ae3f5edca9fe1
xdswwwj <xdswwwj@gamil.com>
2022-07-21 18:47:43 +0900

    UPDATE : cors middleware 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.listen(3000, () => {
   console.log("server start port 3000");
