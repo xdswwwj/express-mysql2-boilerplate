a102b5921c1d1f06ed269b75cf45cd38638e8c03
xdswwwj <xdswwwj@gamil.com>
2022-07-21 20:21:40 +0900

    UPDATE : urlencoded 및 json 객체 받게 설정 처리

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import * as conf from "#configs/conf";
 const app = express();
 
 app.use(cors(conf.corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(3000, () => {
   console.log("server start port 3000");
