6c165b0a89a1faa3f65f297603b6a17c9da58ed0
xdswwwj <xdswwwj@gamil.com>
2022-07-22 14:33:05 +0900

    업데이트 : app.js update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
+// NOTE: node modules
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// NOTE: File Import
 import * as conf from "#configs/conf";
+import * as CONSTANT from "#utils/constants";
+import * as resFormat from "#utils/responseFormat";
+
 const app = express();
 
 app.use(cors(conf.corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// 404
+app.use((req, res, next) => {
+  res.json(resFormat.responsesFormat({ type: CONSTANT.RES_TYPE.FRONT_ERR }));
+});
 
 app.listen(3000, () => {
   console.log("server start port 3000");
