a0aebeab554138cf4eccd9afbd45e6ca8efc3bb6
xdswwwj <xdswwwj@gamil.com>
2022-07-30 13:15:40 +0900

    업데이트 : controller에 service wrapper 적용 테스트

--- src/apis/controllers/user.controllers.js
+++ src/apis/controllers/user.controllers.js
@@ -1,14 +1,20 @@
 import * as CONSTANT from "#utils/constants";
 import * as responseUtils from "#utils/responseFormat";
+import * as wrapper from "#utils/wrapper";
+import * as services from "#services/index";
 
 export const getUser = async (req, res) => {
   try {
+    const result = await wrapper.servicesWrapper({
+      cb: services.user.getUser,
+    });
+
     res.json(
       responseUtils.responsesFormat({
         type: CONSTANT.RES_TYPE.SUCCESS.EXISTS_DATA,
         msg: "Test",
         data: {
-          test: "Hello World!",
+          result,
         },
       })
     );
